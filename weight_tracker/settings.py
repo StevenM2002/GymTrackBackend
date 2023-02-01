eed5160f9fc37ee15c90f72cf42557a21bce74c3
Steven <stevenmai1111@hotmail.com>
2023-02-01 22:51:48 +1100

    updated allowed hosts

--- weight_tracker/settings.py
+++ weight_tracker/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-7g0d5jxw6i@ve=nt$3h2$64+jvu67z6c&1m8+49fm04qkq=g@&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "melodious-profiterole-fe5b06.netlify.app"]
 
 # Application definition
 
